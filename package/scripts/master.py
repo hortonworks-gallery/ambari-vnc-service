9f8793212657c708ef200489556dece6ed5e1941
Ali Bajwa <abajwa@hortonworks.com>
2015-01-05 04:05:14 -0800

    removed creation of eclipse shortcut

--- package/scripts/master.py
+++ package/scripts/master.py
@@ -22,7 +22,7 @@ class Master(Script):
         Execute('wget http://www.gtlib.gatech.edu/pub/eclipse/technology/epp/downloads/release/luna/SR1/eclipse-java-luna-SR1-linux-gtk-x86_64.tar.gz')
         Execute('tar -zxvf eclipse-java-luna-SR1-linux-gtk-x86_64.tar.gz -C /usr/')
         Execute('ln -s /usr/eclipse/eclipse /usr/bin/eclipse')
-	Execute('ln -s /usr/eclipse/eclipse ~/Desktop/eclipse')
+	#Execute('ln -s /usr/eclipse/eclipse ~/Desktop/eclipse')
 
   def configure(self, env):
     import params
