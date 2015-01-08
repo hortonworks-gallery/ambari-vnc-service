263f2c7ceb0f7cb168f0e241892802a97d052a5c
Ali Bajwa <abajwa@hortonworks.com>
2015-01-07 19:08:16 -0800

    also clean up eclipse/intellig

--- remove.sh
+++ remove.sh
@@ -1,2 +1,8 @@
 rm -rf /var/lib/ambari-server/resources/stacks/HDP/2.2/services/VNC
+rm -rf /usr/eclipse
+rm -rf /usr/idea*
+rm -f ~/Desktop/eclipse
+rm -f ~/Desktop/intellij.sh
 service ambari-server restart
+service ambari-agent restart
+
