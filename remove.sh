61d390168d2942fa95d6871ba0599024e093a4b4
Ali Bajwa <abajwa@hortonworks.com>
2015-03-17 17:25:09 -0700

    also remove spark

--- remove.sh
+++ remove.sh
@@ -1,6 +1,7 @@
 rm -rf /var/lib/ambari-server/resources/stacks/HDP/2.2/services/VNC
-rm -rf /usr/eclipse
+rm -rf /usr/eclipse*
 rm -rf /usr/idea*
+rm -rf /usr/spark-1.2*
 rm -f ~/Desktop/eclipse
 rm -f ~/Desktop/intellij.sh
 service ambari-server restart
