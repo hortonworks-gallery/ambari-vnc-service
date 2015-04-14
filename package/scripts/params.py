551f33f5d9b76f73a875481f58f95d9d89bd7cfb
Ali Bajwa <abajwa@hortonworks.com>
2015-04-14 11:09:41 -0700

    removing unneeded imports

--- package/scripts/params.py
+++ package/scripts/params.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-from resource_management.libraries.functions.version import format_hdp_stack_version, compare_versions
 from resource_management import *
 
 # server configurations
