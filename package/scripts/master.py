e96ad9a4969b3775ecc715d5983850bdfcfdefd3
Ali Bajwa <abajwa@hortonworks.com>
2015-06-05 01:30:20 -0700

    update service to allow reconfiguration and install mvn from yum

--- package/scripts/master.py
+++ package/scripts/master.py
@@ -63,7 +63,7 @@ class Master(Script):
     self.configure(env) 
     Execute('/sbin/service   vncserver stop')
     Execute('rm -f /var/lock/subsys/Xvnc', ignore_failures=True)      
-    Execute('rm -f /tmp/.X1-lock', ignore_failures=True)      
+    Execute('rm -f /tmp/.X*', ignore_failures=True)      
     
       
   def start(self, env):
@@ -71,7 +71,7 @@ class Master(Script):
     self.configure(env)
     
     Execute('rm -f /var/lock/subsys/Xvnc', ignore_failures=True)
-    Execute('rm -f /tmp/.X1-lock', ignore_failures=True)      
+    Execute('rm -f /tmp/.X*', ignore_failures=True)      
     
     Execute('/sbin/service   vncserver start')
     time.sleep(5)
